a7cb35f21db6eda66b4f58ee3c4435a472b574d2
Kenny <kcyam@uci.edu>
2026-02-17 00:39:55 -0800

    update about me

--- src/components/aboutmepage.tsx
+++ src/components/aboutmepage.tsx
@@ -15,12 +15,12 @@ class AboutMe extends Component {
             Hi, my name is Kenneth, and I'm a UCI graduate. I majored in
             Computer Game Science. I have worked at the University of
             California, Los Angeles as a software developer since 2021. Making
-            sure that I understand is important, and I will ask to help me help
-            others, from the stakeholder to colleagues. A solution for the users
-            that assists and maintains is what I strive for. I also love
-            listening to music and learning about the history of pop music, so I
-            make sure I listen to what's popular to see how culture and tastes
-            change and how I adapt with the changing climate.
+            stuff to help or entertain people is what I enjoy, because knowing
+            that people's lives are made better because of what I do is a good
+            feeling. I also love listening to music and learning about the
+            history of pop music, so I make sure I listen to what's popular to
+            see how culture and tastes change and how I adapt with the changing
+            climate.
           </p>
         </div>
       </div>
