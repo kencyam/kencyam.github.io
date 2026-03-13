ac5575ecd11b35cdb84270584bd96ca5938751b4
Kenny <kcyam@uci.edu>
2026-03-12 20:59:38 -0700

    convert createbrowserrouter to createhashrouter for Github Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import Root from "./route_organizer/Root";
@@ -19,40 +19,37 @@ import {
 import MusicArticle00001 from "./components/music-blog/music-articles/00001-00100/00001";
 //#endregion
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Root />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "aboutme/",
-          element: <AboutMe />,
-        },
-        {
-          path: "",
-          element: <Landing />,
-        },
-        {
-          path: "projects/",
-          element: <Projects />,
-        },
-        {
-          path: "musicblog/",
-          element: <MusicBlog />,
-          children: [
-            {
-              path: "00001/",
-              element: <MusicArticle00001 />,
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  { basename: "/" },
-);
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "aboutme/",
+        element: <AboutMe />,
+      },
+      {
+        path: "",
+        element: <Landing />,
+      },
+      {
+        path: "projects/",
+        element: <Projects />,
+      },
+      {
+        path: "musicblog/",
+        element: <MusicBlog />,
+        children: [
+          {
+            path: "00001/",
+            element: <MusicArticle00001 />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
